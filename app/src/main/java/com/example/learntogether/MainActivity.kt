a81c74e74d70bb09c786ffdf2a591b8e52159a80
LENOVO <kouamejustin116@gmail.com>
2025-09-13 15:01:20 +0000

    update main app

---
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learntogether.ui.theme.LearnTogetherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LearnTogetherTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TutorialScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TutorialScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        Image(
+            painter = painterResource(id = R.drawable.bg_compose_background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+
+        Text(
+            text = stringResource(id = R.string.title_compose),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+
+        Text(
+            text = stringResource(id = R.string.intro_compose),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+
+
+        Text(
+            text = stringResource(id = R.string.body_compose),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TutorialScreenPreview() {
+    LearnTogetherTheme {
+        TutorialScreen()
+    }
+}
